8b441c6e71f66baef5622738788e1218ad5dce9c
Marek Dworecki <marek.dworecki01@gmail.com>
2026-01-15 21:27:01 +0100

    Removes duplicate function declaration
    Eliminates redundant code to improve maintainability and prevent potential confusion caused by multiple identical function definitions.

--- src/services/defaults.ts
+++ src/services/defaults.ts
@@ -42,18 +42,3 @@ export const getDefaultView = () => {
         },
     }
 }
-
-export const getDefaultView = () => {
-    return {
-        destination: {
-            x: 3841338.2348480183,
-            y: 1178907.9723167613,
-            z: 4941123.403761086,
-        },
-        orientation: {
-            heading: 0.22980001877882295,
-            pitch: -0.8007149279440147,
-            roll: 6.283156544952853,
-        },
-    }
-}
