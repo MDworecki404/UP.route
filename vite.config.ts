c3491939ff1bf7b14d32b1fdd74c361be44401aa
Marek Dworecki <marek.dworecki01@gmail.com>
2026-01-30 18:35:03 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
                 ],
             },
             workbox: {
-                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+                maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff2}'],
             },
         }),
